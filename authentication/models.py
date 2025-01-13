ad3b2761894c3d9f2c5aed980a3eb99d94a40ba2
P1R47E <yosephtesfaye27@gmail.com>
2025-01-13 11:34:47 +0300

    Authentication...

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key = True, default=uuid.uuid4,editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.Charfield(max_length=15, blank=True)
+    is_verified = models.BooleanField(default=False)
+    two_factor_enabled =  models.BooleanField(default=False)
+    last_login_ip = models.GenericIPAddressField(null= True,blank= True)
+    failed_login_attempts = models.PositiveIntegerField(default = 0)
+    account_locked_untill = models.DateTimeField(null=True,blank = True)
